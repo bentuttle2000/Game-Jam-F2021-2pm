c5fbbcb2d4fc7a3e33833fcccba4dd6be4864af0
Daura951 <murphyd6@sunypoly.edu>
2021-11-04 14:52:22 -0400

    Enemies Imported

--- GameJam/Enemy.gd
+++ GameJam/Enemy.gd
@@ -7,26 +7,29 @@ var CurrentPosition = Vector2();
 var velocity = Vector2();
 
 func _process(delta):
-	if get_owner() != null:
-		PlayerPosition = get_tree().current_scene.get_node("Player").get_position();
-	CurrentPosition = get_position();
-	if PlayerPosition.x > CurrentPosition.x:
-		velocity.x = SPEED;
-	elif PlayerPosition.x < CurrentPosition.x:
-		velocity.x = -SPEED;
-	else:
-		velocity.x = 0;
-	if PlayerPosition.y > CurrentPosition.y:
-		velocity.y = SPEED;
-	elif PlayerPosition.y < CurrentPosition.y:
-		velocity.y = -SPEED;
-	else:
-		velocity.y = 0;
-	move_and_slide(velocity);
+	if !is_dead:
+		$AnimatedSprite.play("Idle")
+		if get_owner() != null:
+			PlayerPosition = get_tree().current_scene.get_node("Player").get_position();
+		CurrentPosition = get_position();
+		if PlayerPosition.x > CurrentPosition.x:
+			velocity.x = SPEED;
+		elif PlayerPosition.x < CurrentPosition.x:
+			velocity.x = -SPEED;
+		else:
+			velocity.x = 0;
+		if PlayerPosition.y > CurrentPosition.y:
+			velocity.y = SPEED;
+		elif PlayerPosition.y < CurrentPosition.y:
+			velocity.y = -SPEED;
+		else:
+			velocity.y = 0;
+		move_and_slide(velocity);
 
 
 
 func dead():
+	$AnimatedSprite.play("Dead")
 	is_dead = true;
 	$Timer.start();
 
